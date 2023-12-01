ecca74da13c6c389e369eec2e11f74f70324b022
stratospherra <127473975+stratospherra@users.noreply.github.com>
2023-12-01 11:30:39 +0600

    backend

--- bot_back/shop/models.py
+++ bot_back/shop/models.py
@@ -2,3 +2,11 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
